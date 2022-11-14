291b85336e82116107986137284894b50d229143
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-14 08:56:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,35 +2,35 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 global.hituet = 0
-global.gopayno = "0877-0504-8235"
-global.danano = "0877-0504-8233"
-global.shopeepayno = "0857-9814-5596"
-global.creator = "6287705048235@s.whatsapp.net"
+global.gopayno = "r u sure ? "
+global.danano = "r u sure ? "
+global.shopeepayno = "r u sure ? "
+global.creator = "62859106985947@s.whatsapp.net"
 global.thumb = fs.readFileSync(`./image/thumb.png`)
-global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
-global.fake = `📍𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑲𝒂𝒚𝒍𝒂 𝑩𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑`
+global.qrisdonate = fs.readFileSync(`./image/qrs.jpg`)
+global.fake = `📍𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝓡𝓮𝓮𝔂𝓪 `
 global.packname = ``
-global.author = `×͜× ▭▬▭▬▭▬ 👑 ▭▬▭▬▭▬ ×͜×\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 𝗞𝗮𝘆𝗹𝗮\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 𝗞𝗮𝘆𝗹𝗮 : 𝟬𝟴𝟳𝟳𝟬𝟱𝟬𝟰𝟴𝟮𝟯𝟱\nﾒ 𝗬𝗼𝘂𝘁𝘂𝗯𝗲 : 𝗞𝗶𝗿𝗕𝗼𝘁𝘇\n⊟————————⊟`
+global.author = `×͜× ▭▬▭▬▭▬ 👑 ▭▬▭▬▭▬ ×͜×\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 𝓡𝓮𝓮𝔂𝓪\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 Reeya : 0859106985947\nﾒ 𝗬𝗼𝘂𝘁𝘂𝗯𝗲 : Gapunya xD\n⊟————————⊟`
 global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
 global.autoreadsw = true
-global.ownerNomor = '6287705048235'
-global.ownerName = 'Creator Kayla'
-global.ownerNumber = ["6287705048235@s.whatsapp.net"]
+global.ownerNomor = '62859106985947'
+global.ownerName = 'Creator Reeya'
+global.ownerNumber = ["62859106985947@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
-    wait: 'Wait Sis Please be patient',
-    succes: 'Good Luck Sis ?',
+    wait: 'Wait Please be patient',
+    succes: 'Good Luck ?',
     admin: 'Group Admin Special Features!!!',
-    botAdmin: 'Bots Must Be Admins First!!!',
-    owner: 'Ente Kadang-kadang Ente, Nih Khusus Owner Nih!!!',
+    botAdmin: 'Bot Must Be Admin First!!!',
+    owner: ' Sorry This Feature Only For Owner!!!',
     group: 'Features Used Only For Groups!!!',
     private: 'Features Used Only For Private Chat!!!',
     bot: 'Bot Number User Special Features!!!',
-    error: 'Error Sis, Please Chat Owner...',
-    premium: 'Maaf Sebelumya Kamu Belum Premium, Silahkan Pencet Di Bawah Untuk Beli Premium',
+    error: 'Error , Please Chat Owner...',
+    premium: 'Sorry But You Are Not Premium Yet,  Press This Button Below If You Want To Be  Premium',
 }
 
 global.bapak = [
